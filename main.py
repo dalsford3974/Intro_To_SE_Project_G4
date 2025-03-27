87bbb9d6e9ab75fac891829eca19e71dc3b778cb
dalsford3974 <dalsford397@gmail.com>
2025-03-27 14:13:25 -0500

    Created an initial home page and nav bar

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask
-#from models import db
+from flask import Flask, render_template
+from models import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///introToSE.db'
 db.init_app(app)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+
+if __name__ == "__main__":
+    create_tables()
+    app.run(debug=True)
