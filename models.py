60432ae49cfdec65bbd9ae296e5fcdcbd65b7060
dalsford3974 <dalsford397@gmail.com>
2025-03-27 15:27:40 -0500

    Fixed primary keys

--- models.py
+++ models.py
@@ -5,8 +5,9 @@ from sqlalchemy import Numeric
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__ = 'users' 
+    __tablename__ = 'users'
     userID = Column(Integer, primary_key=True)
     firstName = Column(String(50), nullable=False)
     lastName = Column(String(50), nullable=False)
@@ -16,30 +17,36 @@ class User(db.Model):
     zipCode = Column(String(50), nullable=False)
     isAdmin = Column(Integer, nullable=False)
 
-class Inventory(db.model):
+
+class Inventory(db.Model):
     __tablename__ = 'inventory'
     ItemID = Column(Integer, primary_key=True)
     Title = Column(String(50), nullable=False)
     UserID = Column(Integer, ForeignKey('users.userID'), nullable=False)
-    Price = Column(Numeric(10,2), nullable=False)
+    Price = Column(Numeric(10, 2), nullable=False)
     Stock = Column(Integer, nullable=False)
 
-class Cart(db.model):
+
+class Cart(db.Model):
     __tablename__ = 'cart'
+    CartID = Column(Integer, primary_key=True)
     UserID = Column(Integer, ForeignKey('users.userID'), nullable=False)
     ItemID = Column(Integer, ForeignKey('inventory.ItemID'), nullable=False)
     Quantity = Column(Integer, nullable=False)
 
-class Orders(db.model):
+
+class Orders(db.Model):
     __tablename__ = 'orders'
     OrderID = Column(Integer, primary_key=True)
     UserID = Column(Integer, ForeignKey('users.userID'), nullable=False)
     ItemNumber = Column(Integer, nullable=False)
-    Cost = Column(Numeric(10,2), nullable=False)
+    Cost = Column(Numeric(10, 2), nullable=False)
     Date = Column(String, nullable=False)
 
-class OrderItems(db.model):
+
+class OrderItems(db.Model):
     __tablename__ = 'orderitems'
+    HistoryID = Column(Integer, primary_key=True)
     OrderID = Column(Integer, ForeignKey('orders.OrderID'), nullable=False)
     ItemID = Column(Integer, ForeignKey('inventory.ItemID'), nullable=False)
     Quantity = Column(Integer, nullable=False)
