46476f68a9ff99ea42f34b120e224d256f577c18
dalsford3974 <dalsford397@gmail.com>
2025-03-27 14:34:41 -0500

    Added Login page and

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from models import db, User
 
 app = Flask(__name__)
@@ -10,6 +10,9 @@ db.init_app(app)
 def home():
     return render_template('home.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 def create_tables():
     with app.app_context():
